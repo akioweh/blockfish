2b724d60e710a978ac6622383503bb22d279fa45
akioweh <79527273+akioweh@users.noreply.github.com>
2024-10-21 18:40:14 +0100

    Initial Commit

---
+++ blockfish/__init__.py
@@ -0,0 +1,9 @@
+"""
+Tetris Engine.
+
+Blockfish is an autonomous Tetris player that uses a
+chess-engine-inspired approach to play the game optimally.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Akioweh'
